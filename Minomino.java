14b9bf596087bdd2e69a14a14d1b3d74231b4d2b
Yllnor Rukovci <yllruk13@gmail.com>
2023-05-21 22:51:43 +0200

    added several classes and methods

--- Minomino.java
+++ Minomino.java
@@ -1,11 +1,12 @@
 public class Minomino {
     public String symbol = "[]";
-    public Boolean isFull = false;
-    public Boolean active = true;
+    public Boolean isFull;
+    public Boolean active;
 
 
     public Minomino(boolean isFull) {
         this.isFull = isFull;
+        this.active = isFull;
     }
 
     public String toString(){
@@ -16,7 +17,19 @@ public class Minomino {
         active = !active;
     }
 
+    public void erase(){
+        isFull = false;
+    }
+
     public String getSymbol() {
         return isFull ? symbol : ". ";
+    }
+
+    public Boolean getActive(){
+        return active;
+    }
+
+    public Boolean getIsFull(){
+        return isFull;
     }
 }
---
+++ Otetro.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Otetro implements Tetromino{
+    private List<List<Minomino>> tetro;
+
+    public Otetro(){
+        List<Minomino> layer1 = new ArrayList<>();
+        Minomino m1 = new Minomino(true);
+        Minomino m2 = new Minomino(true);
+        layer1.add(0, m1);
+        layer1.add(1, m2);
+        List<List<Minomino>> oblock = new ArrayList<>();
+        oblock.add(layer1);
+        oblock.add(layer1);
+        
+        tetro = oblock;
+    }
+
+    @Override
+    public List<List<Minomino>> returnTetro() {
+        // TODO Auto-generated method stub
+
+        return tetro;
+    }
+
+    @Override
+    public void rotate() {
+        // TODO make a rotate method for each rotation
+    }
+
+    @Override
+    public void setActive() {
+        for(List<Minomino> list : tetro){
+            for(Minomino mino : list){
+                mino.settle();
+            }
+        }
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setActive'");
+    }
+    
+}
