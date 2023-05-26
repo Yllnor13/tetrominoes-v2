459ea44117ae87fa0dcda4afac9f75cd2dbef1a5
Yllnor Rukovci <yllruk13@gmail.com>
2023-05-26 20:02:42 +0200

    null pointer exception error when ranbut Ineed to go back to a different branch

--- Main.java
+++ Main.java
@@ -1,104 +1,128 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String userinput = scanner.nextLine();
-
+        //shared objects
         Random random = new Random();
 
-        Timer timer = new Timer(); //so that the game advances automatically
-        int intervalInSeconds = 3;
-
         Tetromino square = new Otetro();
-        Tennis field = new Tennis();
+        Tennis tennis = new Tennis();
 
-        int x = random.nextInt(9);
-        int y = 0;
-        
-        while (y < 24) {
+        //thread 1
+        UserInputThread uit = new UserInputThread(tennis);
+        uit.start();
 
-            x = random.nextInt(13);
-            
-            field.drawField();
-            field.insert(square, x, y);
-            field.drawField();
-            field.insert(square, -5, y);
-            field.drawField();
-            field.insert(square, 13, y);
+        //thread 2
+        UpdateTennisThread utt = new UpdateTennisThread(tennis);
+        utt.start();
 
-            y++;
-        
-            try {
-                Thread.sleep(1000); // Delay for 1 second
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try{
+            uit.join();
+            utt.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run(){
-                /*
-                if(x==10){
-                    timer.cancel();
-                }
-                
-                if(userinput == "QUIT"){
-                    timer.cancel();
-                }
-                */
-            }
-        }, intervalInSeconds, intervalInSeconds);
+
+    }
+}
+
+class UserInputThread extends Thread{
+    private Tennis tennis;
+    private Scanner scanner = new Scanner(System.in);
+    private int currentX;
+    private int currentY;
+    private Tetromino currentTetro;
+
+    public UserInputThread(Tennis t){
+        tennis = t;
     }
 
-    class UpdateTennisThread extends Thread{
-        private Tennis tennis;
-        private long time;
-        private int currentX;
-        private int currentY;
-        private Tetromino currentTetro;
+    @Override
+    public void run(){
+        try{
+            String userInput = "";
+            while (!userInput.equalsIgnoreCase("quit")) {
+                currentX = tennis.getPrevX();
+                currentY = tennis.getPrevY();
+                currentTetro = tennis.getPrevTetro();
+                userInput = scanner.nextLine().toLowerCase();
 
-        public UpdateTennisThread(Tennis ten, int x, int y, Tetromino tet){
-            tennis = ten;
-            time = 5000;
-            currentX = x;
-            currentY = y;
-            currentTetro = tet;
-        }
-
-        public void updateTetro(Tetromino t){
-            currentTetro = t;
-        }
-
-        public void updateX(int x){
-            currentX = x;
-        }
-
-        public void updateY(int y){
-            currentY = y;
-        }
-
-        @Override
-        public void run(){
-            try{
-                while (time >= 500){
-                    Thread.sleep(time);
-
-                    updateX(tennis.getPrevX());
-                    updateY(tennis.getPrevY());
-                    currentY--;
-
-                    tennis.insert(null, currentX, currentY);
-
-                    time = time-10;
+                switch (userInput) {
+                    case "a":
+                        currentX--;
+                        tennis.insert(currentTetro, currentX, currentY);
+                        break;
+                    case "d":
+                        currentX++;
+                        tennis.insert(currentTetro, currentX, currentY);
+                        break;
+                    case "s":
+                        currentY++;
+                        tennis.insert(currentTetro, currentX, currentY);
+                    case "x":
+                        currentY = tennis.HEIGHT;
+                        tennis.insert(currentTetro, currentX, currentY);
+                        break;
+                    case "r":
+                        currentTetro.rotate();
+                        tennis.insert(currentTetro, currentX, currentY);
+                    default:
+                        // Handle unknown command or invalid input
+                        System.out.println("Unknown command: " + userInput);
+                        break;
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
+    }
+}
+
+class UpdateTennisThread extends Thread{
+    private Tennis tennis;
+    private long time;
+    private int currentX;
+    private int currentY;
+    private Tetromino currentTetro;
+
+    public UpdateTennisThread(Tennis ten){
+        tennis = ten;
+        time = 5000;
+    }
+
+    public void updateTetro(Tetromino t){
+        currentTetro = t;
+    }
+
+    public void updateX(int x){
+        currentX = x;
+    }
+
+    public void updateY(int y){
+        currentY = y;
+    }
+
+    @Override
+    public void run(){
+        try{
+            while (time >= 500){
+                Thread.sleep(time);
+
+                updateX(tennis.getPrevX());
+                updateY(tennis.getPrevY());
+                updateTetro(tennis.getPrevTetro());
+                currentY--;
+
+                tennis.insert(null, currentX, currentY);
+
+                time = time-10;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
