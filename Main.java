30a969f0b450b60a954b731353d9ad9d2f11df1b
Yllnor Rukovci <yllruk13@gmail.com>
2023-05-26 22:52:58 +0200

    added new objects

--- Main.java
+++ Main.java
@@ -8,50 +8,44 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         //String userinput = scanner.nextLine();
-
         Random random = new Random();
-
-        Timer timer = new Timer(); //so that the game advances automatically
-        int intervalInSeconds = 3;
-
-        Tetromino square = new Otetro();
-        Tennis field = new Tennis();
-
-        int x = random.nextInt(9);
-        int y = 0;
+        Tetromino currentTetro;
         
-        while (y < 24) {
 
-            x = random.nextInt(13);
-            
-            field.drawField();
-            field.insert(square, x, y);
-            field.drawField();
-            field.insert(square, -5, y);
-            field.drawField();
-            field.insert(square, 13, y);
+        Tennis tennis = new Tennis();
+        int currentX;
+        int currentY;
+        String userInput = "";
+        while (!userInput.equalsIgnoreCase("quit")) {
+            currentX = tennis.getPrevX();
+            currentY = tennis.getPrevY();
+            currentTetro = tennis.getPrevTetro();
+            userInput = scanner.nextLine().toLowerCase();
 
-            y++;
-        
-            try {
-                Thread.sleep(1000); // Delay for 1 second
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            switch (userInput) {
+                case "a":
+                    currentX--;
+                    tennis.insert(currentTetro, currentX, currentY);
+                    break;
+                case "d":
+                    currentX++;
+                    tennis.insert(currentTetro, currentX, currentY);
+                    break;
+                case "s":
+                    currentY++;
+                    tennis.insert(currentTetro, currentX, currentY);
+                case "x":
+                    currentY = tennis.HEIGHT;
+                    tennis.insert(currentTetro, currentX, currentY);
+                    break;
+                case "r":
+                    currentTetro.rotate();
+                    tennis.insert(currentTetro, currentX, currentY);
+                default:
+                    // Handle unknown command or invalid input
+                    System.out.println("Unknown command: " + userInput);
+                    break;
             }
         }
-        
-        timer.scheduleAtFixedRate(new TimerTask() {
-            public void run(){
-                /*
-                if(x==10){
-                    timer.cancel();
-                }
-                
-                if(userinput == "QUIT"){
-                    timer.cancel();
-                }
-                */
-            }
-        }, intervalInSeconds, intervalInSeconds);
     }
 }
--- Tetromino.java
+++ Tetromino.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public interface Tetromino {
-    List<List<Minomino>> brick = new ArrayList<>();
     public List<List<Minomino>> returnTetro();
     public void rotate();
     public void setActive();
