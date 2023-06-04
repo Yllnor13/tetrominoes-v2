f77519772f7dabab82d0ea9afb0129b63d8adaa5
Yllnor Rukovci <yllruk13@gmail.com>
2023-06-04 14:52:56 +0200

    fixed lineclear, altered insert, added more tetrominoes

--- Main.java
+++ Main.java
@@ -12,164 +12,54 @@ public class Main {
         Tetromino currentTetro = new Itetro();
         Tetromino currentTetro2 = new Otetro();
 
-        currentTetro2.setActive();
 
         
         Tennis tennis = new Tennis();
         int currentX;
         int currentY;
         String userInput = "";
-        currentTetro.rotate();
 
-        
-        int x = 0;
-        int y = 13;
-
-        while(tennis.insert(currentTetro, x, y) == true){
-            y++;
-            if(tennis.insert(currentTetro, x, y)){
-                tennis.insert(currentTetro, x, y);
-                tennis.drawField();
+        int y = 0;
+        for(int x = 0; x < tennis.WIDTH;x++){
+            if(x<8){
+                while(tennis.insert(currentTetro, x, y) == true){
+                    y++;
+                    if(tennis.insert(currentTetro, x, y)){
+                        tennis.insert(currentTetro, x, y);
+                        tennis.drawField();
+                    }
+                    else{
+                        break;
+                    }
+                    try{
+                        Thread.sleep(150);
+                    }catch(Exception e){
+                        System.out.println("shit happens");
+                    }
+                }
             }
             else{
-                break;
+                while(tennis.insert(currentTetro2, x, y) == true){
+                    y++;
+                    if(tennis.insert(currentTetro2, x, y)){
+                        tennis.insert(currentTetro2, x, y);
+                        tennis.drawField();
+                    }
+                    else{
+                        break;
+                    }
+                    try{
+                        Thread.sleep(50);
+                    }catch(Exception e){
+                        System.out.println("shit happens");
+                    }
+                }
             }
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        x=4;
-        y=13;
-
-        while(tennis.insert(currentTetro, x, y) == true){
-            y++;
-            if(tennis.insert(currentTetro, x, y)){
-                tennis.insert(currentTetro, x, y);
-                tennis.drawField();
-            }
-            else{
-                break;
-            }
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        x=8;
-        y=13;
-
-        while(tennis.insert(currentTetro2, x, y) == true){
-            y++;
-            if(tennis.insert(currentTetro2, x, y)){
-                tennis.insert(currentTetro2, x, y);
-                tennis.drawField();
-            }
-            else{
-                break;
-            }
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        tennis.lineClear();
-        tennis.drawField();
-
-        /*
-        Boolean test = true;
-
-        int test1=14;
-        int test2=17;
-
-        while(test){
-            test = false;
-            int test3=test1+test2;
-            System.out.println(test3);
-        }
-
-        while(tennis.insert(currentTetro, x, y) == true){
-            y++;
-            if(tennis.insert(currentTetro, x, y)){
-                tennis.insert(currentTetro, x, y);
-                tennis.drawField();
-                currentTetro.rotate();
-            }
-            else{
-                break;
-            }
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        y=14;
-        Tetromino newTetro = new Otetro();
-        tennis.drawField();
-        x++;
-
-        while(tennis.insert(newTetro, x, y) == true){
-            if(tennis.insert(newTetro, x, y)){
-                tennis.insert(newTetro, x, y);
-                tennis.drawField();
-            }
-            else{
-                break;
-            }
-            y++;
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        y=14;
-        tennis.drawField();
-
-        while(tennis.insert(newTetro, 7, y) == true){
-            if(tennis.insert(newTetro, 7, y)){
-                tennis.insert(newTetro, 7, y);
-                tennis.drawField();
-
-            }
-            else{
-                break;
-            }
-            y++;
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
-        }
-
-        y=14;
-        tennis.drawField();
-
-        while(tennis.insert(newTetro, 7, y) == true){
-            if(tennis.insert(newTetro, 7, y)){
-                tennis.insert(newTetro, 7, y);
-                tennis.drawField();
-            }
-            y++;
-            try{
-                Thread.sleep(500);
-            }catch(Exception e){
-                System.out.println("shit happens");
-            }
+            tennis.drawField();
+            y=0;
         }
 
         tennis.drawField();
 
-        */
     }
 }
